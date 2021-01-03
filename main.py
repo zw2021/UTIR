e331c0ba617ca8c91da1443aae5b5be74b632ea9
zw2021 <zoelle.wong@utexas.edu>
2021-01-02 19:46:55 -0600

    first commit!

---
+++ main.py
@@ -0,0 +1,275 @@
+from string import punctuation
+from collections import Counter
+from torch.utils.data import DataLoader, TensorDataset
+
+import numpy as np
+import torch as nn
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import glob
+import os
+import torch
+
+#== Functions
+# Truncating data set
+def pad_features(reviews_int, seq_length):
+    ''' Return features of review_ints, where each review is padded with 0's or truncated to the input seq_length.
+    '''
+    features = np.zeros((len(reviews_int), seq_length), dtype=int)
+
+    for i, review in enumerate(reviews_int):
+        review_len = len(review)
+
+        if review_len <= seq_length:
+            zeroes = list(np.zeros(seq_length - review_len))
+            new = zeroes + review
+        elif review_len > seq_length:
+            new = review[0:seq_length]
+
+        features[i, :] = np.array(new)
+
+    return features
+
+
+
+#== classes
+class SentimentLSTM(torch.nn.Module):
+    """
+    The RNN model that will be used to perform Sentiment analysis.
+    """
+
+    def __init__(self, vocab_size, output_size, embedding_dim, hidden_dim, n_layers, drop_prob=0.5):
+        """
+        Initialize the model by setting up the layers.
+        """
+        super().__init__()
+
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+
+        # embedding and LSTM layers
+        self.embedding = torch.nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = torch.nn.LSTM(embedding_dim, hidden_dim, n_layers,
+                            dropout=drop_prob, batch_first=True)
+
+        # dropout layer
+        self.dropout = torch.nn.Dropout(0.3)
+
+        # linear and sigmoid layers
+        self.fc = torch.nn.Linear(hidden_dim, output_size)
+        self.sig = torch.nn.Sigmoid()
+
+    def forward(self, x, hidden):
+        """
+        Perform a forward pass of our model on some input and hidden state.
+        """
+        batch_size = x.size(0)
+
+        # embeddings and lstm_out
+        embeds = self.embedding(x)
+        lstm_out, hidden = self.lstm(embeds, hidden)
+
+        # stack up lstm outputs
+        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+
+        # dropout and fully-connected layer
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+        # sigmoid function
+        sig_out = self.sig(out)
+
+        # reshape to be batch_size first
+        sig_out = sig_out.view(batch_size, -1)
+        sig_out = sig_out[:, -1]  # get last batch of labels
+
+        # return last sigmoid output and hidden state
+        return sig_out, hidden
+
+    def init_hidden(self, batch_size):
+        ''' Initializes hidden state '''
+        # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
+        # initialized to zero, for hidden state and cell state of LSTM
+        weight = next(self.parameters()).data
+
+        if (train_on_gpu):
+            hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda(),
+                      weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda())
+        else:
+            hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
+                      weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
+
+        return hidden
+#== extract files from tarfile
+# tar = tarfile.open("aclImdb_v1.tar.gz")
+# tar.extractall("./text")
+# tar.close()
+
+#== tokenize text
+# collect all text files in the negative review direcotry
+reviews = []
+path = "C:/Users/huang/Documents/pythonUTIR/text/aclImdb/test/neg/*"
+
+for file in glob.glob(path):
+    with open(file, encoding="cp437") as f:
+        neg_text = f.read()
+
+    neg_text = neg_text.lower()
+    neg_text = ''.join([c for c in neg_text if c not in punctuation])
+    reviews.append(neg_text)
+
+all_text2 = ' '.join(reviews)
+words = all_text2.split()
+count_words = Counter(words)
+
+total_words = len(words)
+sorted_words = count_words.most_common(total_words)
+vocab_to_int = {w:i+1 for i, (w,c) in enumerate(sorted_words)}  # vocab to int mapping dictionary
+encoded_labels = [0]*len(reviews) #LSTM tutorial said 1 for positive, but 0 for negative. These are all negative reviews, so we will make an array of 0s
+
+reviews_int = []
+for review in reviews:
+    r = [vocab_to_int[w] for w in review.split()]
+    reviews_int.append(r)
+
+
+reviews_len = [len(x) for x in reviews_int]
+pd.Series(reviews_len).hist()
+plt.title('Histogram for Lengths of Reviews')
+#print(pd.Series(reviews_len).describe())
+plt.show()
+
+reviews_int = [ reviews_int[i] for i, l in enumerate(reviews_len) if l>0 ]
+encoded_labels = [ encoded_labels[i] for i, l in enumerate(reviews_len) if l> 0 ]
+
+features = pad_features(reviews_int, seq_length=200) # tutorial used 200
+split_frac = 0.8
+len_feat = 50 # dummy value
+
+train_x = features[0:int(split_frac*len_feat)]
+train_y = encoded_labels[0:int(split_frac*len_feat)]
+remaining_x = features[int(split_frac*len_feat):]
+remaining_y = encoded_labels[int(split_frac*len_feat):]
+valid_x = remaining_x[0:int(len(remaining_x)*0.5)]
+valid_y = remaining_y[0:int(len(remaining_y)*0.5)]
+test_x = remaining_x[int(len(remaining_x)*0.5):]
+test_y = remaining_y[int(len(remaining_y)*0.5):]
+
+
+train_data = TensorDataset(torch.from_numpy(np.array(train_x)), torch.from_numpy(np.array(train_y)))
+valid_data = TensorDataset(torch.from_numpy(np.array(valid_x)), torch.from_numpy(np.array(valid_y)))
+test_data = TensorDataset(torch.from_numpy(np.array(test_x)), torch.from_numpy(np.array(test_y)))
+
+# dataloaders
+batch_size = 50
+# SHUFFLE data
+train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
+valid_loader = DataLoader(valid_data, shuffle=True, batch_size=batch_size)
+test_loader = DataLoader(test_data, shuffle=True, batch_size=batch_size)
+
+# obtain one batch of training data
+dataiter = iter(train_loader)
+sample_x, sample_y = dataiter.next()
+print('Sample input size: ', sample_x.size()) # batch_size, seq_length
+print('Sample input: \n', sample_x)
+print()
+print('Sample label size: ', sample_y.size()) # batch_size
+print('Sample label: \n', sample_y)
+
+
+# Instantiate the model w/ hyperparams
+vocab_size = len(vocab_to_int)+1 # +1 for the 0 padding
+output_size = 1
+embedding_dim = 400
+hidden_dim = 256
+n_layers = 2
+net = SentimentLSTM(vocab_size, output_size, embedding_dim, hidden_dim, n_layers)
+
+SentimentLSTM(
+  (embedding): Embedding(74073, 400)
+  (lstm): LSTM(400, 256, num_layers=2, batch_first=True, dropout=0.5)
+  (dropout): Dropout(p=0.3)
+  (fc): Linear(in_features=256, out_features=1, bias=True)
+  (sig): Sigmoid()
+)
+
+# loss and optimization functions
+lr=0.001
+
+criterion = nn.BCELoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+
+
+# training params
+
+epochs = 4 # 3-4 is approx where I noticed the validation loss stop decreasing
+
+counter = 0
+print_every = 100
+clip=5 # gradient clipping
+
+# move model to GPU, if available
+if(train_on_gpu):
+    net.cuda()
+
+net.train()
+# train for some number of epochs
+for e in range(epochs):
+    # initialize hidden state
+    h = net.init_hidden(batch_size)
+
+    # batch loop
+    for inputs, labels in train_loader:
+        counter += 1
+
+        if(train_on_gpu):
+            inputs, labels = inputs.cuda(), labels.cuda()
+
+        # Creating new variables for the hidden state, otherwise
+        # we'd backprop through the entire training history
+        h = tuple([each.data for each in h])
+
+        # zero accumulated gradients
+        net.zero_grad()
+
+        # get the output from the model
+        inputs = inputs.type(torch.LongTensor)
+        output, h = net(inputs, h)
+
+        # calculate the loss and perform backprop
+        loss = criterion(output.squeeze(), labels.float())
+        loss.backward()
+        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+        nn.utils.clip_grad_norm_(net.parameters(), clip)
+        optimizer.step()
+
+        # loss stats
+        if counter % print_every == 0:
+            # Get validation loss
+            val_h = net.init_hidden(batch_size)
+            val_losses = []
+            net.eval()
+            for inputs, labels in valid_loader:
+
+                # Creating new variables for the hidden state, otherwise
+                # we'd backprop through the entire training history
+                val_h = tuple([each.data for each in val_h])
+
+                if(train_on_gpu):
+                    inputs, labels = inputs.cuda(), labels.cuda()
+
+                inputs = inputs.type(torch.LongTensor)
+                output, val_h = net(inputs, val_h)
+                val_loss = criterion(output.squeeze(), labels.float())
+
+                val_losses.append(val_loss.item())
+
+            net.train()
+            print("Epoch: {}/{}...".format(e+1, epochs),
+                  "Step: {}...".format(counter),
+                  "Loss: {:.6f}...".format(loss.item()),
+                  "Val Loss: {:.6f}".format(np.mean(val_losses)))
+
+
+print(('hi'))
